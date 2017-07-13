08219e125edafc323cdf7048a0965e87099e08b6
Dejan Saric <dsaric@mail.usf.edu>
2017-07-13 16:07:21 -0400

    Complete

--- main.js
+++ main.js
@@ -5,15 +5,44 @@
    to watch the assertions pass as you write your code.
 
    Also remember, that the parameter `hand` will be an array, so
-   you'll need to parse through that first before you can start to 
+   you'll need to parse through that first before you can start to
    write your logic.
 */
 
 
+
+
 function handValue (hand) {
 
+  let currentHand = 0;
 
-  return;
+  for (var i = 0; i < hand.length; i++) {
+    let card = hand[i];
+
+    if (card === "J" || card === "Q" || card === "K") {
+      currentHand = currentHand + 10;
+    }
+
+
+
+    else if (card !== "J" && card !== "Q" && card !== "K" && card !== "A") {
+
+      currentHand = currentHand + Number(card);
+    }
+
+    else if (currentHand < 11 && card === "A") {
+      currentHand = currentHand + 11;
+    }
+
+    else if (currentHand > 11 && card === "A") {
+      currentHand = currentHand + 1;
+    }
+
+
+
+  }
+  console.log(currentHand);
+  return currentHand;
 }
 
 
